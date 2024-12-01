092166af712f0846fd8c58e3354811b9579f89d9
Ryan Smith <22605441+r-smith@users.noreply.github.com>
2024-11-30 22:44:23 -0800

    Revise

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   title: 'Welcome To The Site!',
 
   // Set the production url of your site here
-  url: 'https://r-smith.github.io',
+  url: 'https://docs.deceptifeed.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/test-docu/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
